137a0a34d1b2d22fc0de4de38e1455100c7a07bf
Leelena Asfaw <leelena.asfaw@gmail.com>
2018-09-30 16:30:55 +0000

    automatically backed up by learn

---
+++ lib/turn.rb
@@ -0,0 +1,27 @@
+def display_board()
+board = [" "," "," "," "," "," "," "," "," "]
+display_board(board)
+end
+
+
+def valid_move(board, input_to_index)
+end
+  
+def position_taken
+end
+
+def move(board, input_to_index, X)
+end
+
+  
+def turn(board)
+  puts "Please enter 1-9:"
+  input_to_index= 
+convert input to index
+if index = true
+  make the move for input
+else
+  puts "Please enter an input"
+end
+  
+end
